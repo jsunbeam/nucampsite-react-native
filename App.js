cb607db0324f78d8b0b952993b2fc121cd19d84a
Jessica Shao <jshaodev@gmail.com>
2023-09-13 12:31:00 -0500

    Navigation Part 1

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Main from "./screens/MainComponent";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
-  return <Main />;
+  return (
+    <NavigationContainer>
+      <Main />
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,8 +1,8 @@
 import RenderCampsite from "../features/campsites/RenderCampsite";
 
-const CampsiteInfoScreen = (props) => {
-  //   console.log("props", props);
-  return <RenderCampsite campsite={props.campsite} />;
+const CampsiteInfoScreen = ({ route }) => {
+  const { campsite } = route.params;
+  return <RenderCampsite campsite={campsite} />;
 };
 
 export default CampsiteInfoScreen;
