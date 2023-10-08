a82bfe5640db1e514775ee459e67f00869b7261d
Jessica Shao <jshaodev@gmail.com>
2023-10-08 18:12:59 -0500

    Week 4 Task 1

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,6 +4,7 @@ import { CheckBox, Input, Button, Icon } from "react-native-elements";
 import * as SecureStore from "expo-secure-store";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as ImagePicker from "expo-image-picker";
+import * as ImageManipulator from "expo-image-manipulator";
 import { baseUrl } from "../shared/baseUrl";
 import logo from "../assets/images/logo.png";
 
@@ -147,9 +148,19 @@ const RegisterTab = () => {
       });
       if (capturedImage.assets) {
         console.log(capturedImage.assets[0]);
-        setImageUrl(capturedImage.assets[0].uri);
+        processImage(capturedImage.assets[0].uri);
       }
     }
+  };
+
+  const processImage = async (imgUri) => {
+    const processedImage = await ImageManipulator.manipulateAsync(
+      imgUri,
+      [{ resize: { width: 400 } }],
+      { format: "png" }
+    );
+    console.log("processedImage", processedImage);
+    setImageUrl(processedImage.uri);
   };
 
   return (
